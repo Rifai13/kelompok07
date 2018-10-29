dfed0dd89571fb91ff299ae6fede8586cf893425
Rifai13 <muh.rifai11@gmail.com>
2018-10-29 08:08:05 +0700

    upload pertama

---
+++ src/snapappo7/SnapAPPo7.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapappo7;
+
+/**
+ *
+ * @author asus
+ */
+public class SnapAPPo7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+SnapFrame tampil = new SnapFrame();
+tampil.setVisible(true);
+// TODO code application logic here
+    }
+    
+}
